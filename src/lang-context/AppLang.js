02181ccc69ef659c8a36a361689e244614c7c21b
Anwar Montasir <anwarmontasir@gmail.com>
2019-06-23 23:55:48 -0700

    child

--- src/lang-context/AppLang.js
+++ src/lang-context/AppLang.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Child from './Child';
 
 export default class AppLang extends Component {
     render() {
         return (
             <div className='AppLang'>
-                App context
+                <Child />
             </div>
         )
     }
