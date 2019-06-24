85ee15d22c39bf8026a2b5b727ce64b3a6b2e19a
Anwar Montasir <anwarmontasir@gmail.com>
2019-06-24 16:32:13 -0700

    deeply nested component updates context

--- src/lang-context/LanguageContext.js
+++ src/lang-context/LanguageContext.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 /* find preferred language of user */
 const LanguageContext = React.createContext({
-    lang: window.navigator.language
+    lang: window.navigator.language,
+    setLang: () => {
+        
+    }
 });
 
 export default LanguageContext;
